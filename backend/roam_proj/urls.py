7ae810bd3c615c6e7ec5d1f92ef8278a72cf03f9
philliphall131 <phillip.hall131@gmail.com>
2022-05-21 13:21:50 -0400

    initialized

---
+++ backend/roam_proj/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .view import send_the_homepage
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', send_the_homepage),
+    path('api/', include('roam_app.urls')),
+    re_path(r'.*', send_the_homepage), #re path so react_router can do its thing
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
